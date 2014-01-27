df8eab526ea5f7eb925311d12a03e08f831a6d60
youngor <youngorchen@gmail.com>
2014-01-27 16:00:07 +0800

    sync

--- MyCAT/src/main/assembly/testtool/test_stand_select_perf.sh
+++ MyCAT/src/main/assembly/testtool/test_stand_select_perf.sh
@@ -15,4 +15,4 @@ CURR_DIR=`pwd`
 cd ..
 MYCAT_HOME=`pwd`
 cd $CURR_DIR
-$JAVA_CMD -Xms256M -Xmx1G -XX:MaxPermSize=64M  -DMYCAT_HOME=$MYCAT_HOME -cp "$MYCAT_HOME/conf:$MYCAT_HOME/lib/*" $MAIN_CLASS $1 $2 $3 $4 $5 $6 $7
+$JAVA_CMD -Xms256M -Xmx1G -XX:MaxPermSize=64M  -DMYCAT_HOME=$MYCAT_HOME -cp "$MYCAT_HOME/conf:$MYCAT_HOME/lib/*" $MAIN_CLASS $1 $2 $3 $4 $5 $6 $7 $8 $9
