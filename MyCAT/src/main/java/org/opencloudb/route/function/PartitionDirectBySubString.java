208581a4091d2a7a0241db7ed169f07f2e8def62
youngor <youngorchen@gmail.com>
2014-03-03 11:00:08 +0800

    sync

---
+++ MyCAT/src/main/java/org/opencloudb/route/function/PartitionDirectBySubString.java
@@ -0,0 +1,30 @@
+package org.opencloudb.route.function;
+
+import org.opencloudb.config.model.rule.RuleAlgorithm;
+
+/**
+ * Created by bingoohuang on 14-2-28.
+ */
+public class PartitionDirectBySubString implements RuleAlgorithm {
+    private int startIndex;
+    private int size;
+
+    public void setStartIndex(String str) {
+        startIndex = Integer.parseInt(str);
+    }
+
+    public void setSize(String str) {
+        size = Integer.parseInt(str);
+    }
+
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public Integer calculate(String columnValue) {
+        return Integer.parseInt(columnValue.substring(startIndex, startIndex + size));
+    }
+}
